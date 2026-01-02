77d223842e38c547667b2ba61ec321b72ab7e952
gabrielcamen <gabrielcamen@gmail.com>
2026-01-02 23:39:00 +0200

    Added first seven chapters references, first chapters need to be organized in functions

---
+++ main.c
@@ -0,0 +1,295 @@
+//
+// Created by Gabe C on 31.12.2025.
+//
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int my_strlen(char *s);
+void SevenStrings();
+void double_array(int *f, int len);
+void print_continues(void);
+void print_spaces(void);
+void SixArrays(void);
+void increment(int *pointer);
+//function prototype, can be defined here and implementation later
+int plus_one(int n);
+//if you dont type void when there is 0 arguments then the compiler might not give you errors if this is called with a parameter
+//In modern C, void foo(void) means “no arguments”, while void foo() means “I don’t know what arguments”, so always use void.
+void print_hello(void);
+void print_number_and_pointer(int someNumber, int* pointerToSomeNumber, int **pointToSomeNumberByReference);
+void passing_array_to_function_size_of(float[4]);
+void pass_array_by_reference(int *f, int len);
+void pass_array_by_notation(int f[], int len);
+
+int main(void) {
+    //3. Variables and statements
+    int i = 0;
+    float f = 3.1414;
+    char *s = "Hello World!";
+    printf("String value is: %s, Float value is %f, int value is %d\n", s, f, i);
+    bool myValue = true;
+    if (myValue == true) {
+        printf("Bools work with stdbool.h\n");
+    }else {
+        printf("Bools do not work with stdbool.h\n");
+    }
+
+    int a = 999;
+    printf("%zu\n", sizeof(a));
+    printf("%zu\n", sizeof(-a));
+    printf("%zu\n", sizeof(2 + 7));
+    printf("%zu\n", sizeof(3.14));
+
+    //basic loop that can be done like this
+    while (i < 10) {
+        printf("i is now: %d\n", i);
+        i++;
+    }
+
+    //you can infinte loop and break out of it
+    while (1) {
+        if (i == 20) {
+            break;
+        }
+        printf("i is now: %d\n", i);
+        i++;
+    }
+
+    for (i = 0; i < 10; i++) {
+        printf("i is now in for loop: %d\n", i);
+    }
+    /*
+    for (;;) {
+        printf("This will run infinetely until you hit CTRL-C.\n");
+    }
+    */
+
+    char character = 'b';
+    switch (character) {
+        case 'a':
+            printf("a was pressed\n");
+            break;
+        case 'b':
+            printf("b was pressed\n");
+            break;
+        case 'c':
+            printf("c was pressed\n");
+            break;
+    }
+    //4. Functions
+    int value = 5;
+    printf("value is: %d\n", plus_one(value));
+    print_hello();
+
+    //5. Pointers
+    int someNumber = 25;
+    int* pointerToSomeNumber = &someNumber;
+    print_number_and_pointer(someNumber, pointerToSomeNumber, &pointerToSomeNumber);
+
+    someNumber += 42;
+    print_number_and_pointer(someNumber, pointerToSomeNumber, &pointerToSomeNumber);
+    *pointerToSomeNumber += 55;
+    print_number_and_pointer(someNumber, pointerToSomeNumber, &pointerToSomeNumber);
+    //increasing the pointer itself makes it point to some random garbage location that has nothing in it
+    pointerToSomeNumber += 100;
+    print_number_and_pointer(someNumber, pointerToSomeNumber, &pointerToSomeNumber);
+    *pointerToSomeNumber += 55;
+    print_number_and_pointer(someNumber, pointerToSomeNumber, &pointerToSomeNumber);
+    increment(&someNumber);
+    print_number_and_pointer(someNumber, pointerToSomeNumber, &pointerToSomeNumber);
+
+    int* nullPointer = NULL;
+    printf("Null pointer is %p\n", (void*)nullPointer);
+    //seg fault if you dereference null pointer
+    //*nullPointer += 1;
+
+    //if this is not initialized this will point to garbage or null pointer
+    int* garbagePointer;
+    printf("Garbage pointer is %p\n", (void*)garbagePointer);
+
+    //p is a int*, q is just an int
+    int *p, q;
+    //z is a int*, x is just an int, same declaration as above
+    int* z, x;
+
+    int d = 23;
+    int *dPointer = &d;
+    printf("Size of pointer dpointer: %zu\n", sizeof(dPointer));
+    printf("Size of value dpointer points to %zu\n", sizeof(*dPointer));
+
+    //6. Arrays
+    SixArrays();
+    //7. Strings
+    SevenStrings();
+}
+
+int plus_one(int n) {
+    return n + 1;
+}
+
+void print_hello() {
+    printf("Hello World!\n");
+}
+
+void increment(int *pointer) {
+    *pointer = *pointer + 1;
+}
+
+void print_number_and_pointer(int someNumber, int* pointerToSomeNumber, int **pointToSomeNumberByReference) {
+    print_spaces();
+    printf("The value of someNumber is: %d\n", someNumber);
+    printf("The address of pointerToSomeNumber is %p\n",  (void *)pointerToSomeNumber);
+    printf("The address of pointToSomeNumberByReference is %p\n",  (void *)pointToSomeNumberByReference);
+    printf("The value of pointer to some number:%d\n", *pointerToSomeNumber);
+    print_spaces();
+}
+
+void print_spaces() {
+    printf("--------------------------------\n");
+}
+
+void print_continues() {
+    printf("==============================>\n");
+}
+
+void SixArrays() {
+    print_spaces();
+    printf("6. Arrays\n");
+    //size of the array must be fixed.
+    float f[4];
+
+    f[0] = 3.1415;
+    f[1] = 2.3232;
+    f[2] = 5.1241;
+    f[3] = 2451.51;
+
+    for (int i = 0; i < 4; i++) {
+        printf("%f\n", f[i]);
+    }
+    //how to get the size of an array in the scope, this won't work if you are passing it to a function
+    constexpr size_t total_size = sizeof(f);
+    constexpr size_t element_size = sizeof(f[0]);
+    printf("Size of all elements: %zu\n", total_size);
+    printf("Size of one element: %zu\n", element_size);
+    printf("How many elements: %zu\n", total_size / element_size);
+    passing_array_to_function_size_of(f);
+    print_continues();
+    printf("Size needed for a double array of 48 elements: %zu\n", sizeof(double[48]));
+    print_continues();
+    // Initialize array in one line
+    int a[5] = {22, 37, 3490, 18, 95};
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", a[i]);
+    }
+    print_continues();
+    //Make the first element zero, and then automatically make the rest zero, as well
+    int z[100] = {0};
+    //element at position 5 is going to be 55, rest is 0 then the ones define
+    int my[10] = {0, 11, 22, [5]=55, 66, 77};
+    //you do not need to specify the size explicit
+    int xz[] = {0, 11, 22, [5]=55, 66, 77};
+    //you can exceed the element of an array and go overboard it will just print what is at that data adress
+    for (int i = 0; i < 15; i++) {
+        printf("Size of one element: %d\n", xz[i]);
+    }
+    print_continues();
+    int row, col;
+    int v[2][5] = {
+        {0, 1, 2, 3, 4},
+        {5, 6, 7, 8, 9}
+    };
+    for (row = 0; row < 2; row++) {
+        for (col = 0; col < 5; col++) {
+            printf("(%d,%d) = %d\n", row, col, v[row][col]);
+        }
+    }
+    print_continues();
+    // Make a 3x3 identity matrix
+    int identity[3][3] = {[0][0]=1, [1][1]=1, [2][2]=1};
+    for (row = 0; row < 3; row++) {
+        for (col = 0; col < 3; col++) {
+            printf("(%d,%d) = %d\n", row, col, identity[row][col]);
+        }
+    }
+    print_continues();
+    pass_array_by_reference(a, 5);
+    print_continues();
+    pass_array_by_notation(a, 5);
+    print_continues();
+    int stackInt = 5;
+    //even if the method is defined as an array, you can pass anything that is a pointer to the value of that array and it will just iterate through that memory space
+    pass_array_by_notation(&stackInt, 5);
+    print_continues();
+    int mv[5] = {1, 2, 3, 4, 5};
+    double_array(mv, 5);
+    for (int i = 0; i < 5; i++)
+        printf("%d\n", mv[i]);
+    print_continues();
+}
+
+void passing_array_to_function_size_of(float f[4]) {
+    //because this is passed this actually points to the pointer of the first element in the list
+    constexpr size_t total_size = sizeof(f);
+    constexpr size_t element_size = sizeof(f[0]);
+    printf("Size of all elements: %zu\n", total_size);
+    printf("Size of one element: %zu\n", element_size);
+    printf("How many elements: %zu\n", total_size / element_size);
+}
+
+void pass_array_by_reference(int *f, int len) {
+    for (int i = 0; i < len; i++) {
+        printf("%d\n", f[i]);
+    }
+}
+
+void pass_array_by_notation(int f[], int len) {
+    for (int i = 0; i < len; i++) {
+        printf("%d\n", f[i]);
+    }
+}
+
+void double_array(int *f, int len) {
+    for (int i = 0; i < len; i++) {
+            f[i] *= 2;
+    }
+}
+
+void SevenStrings() {
+    //This form points to wherever that string was placed. Typically, that place is in a land faraway from the rest of your program’s memory – read-only memory – for reasons related to performance & safety.
+    char *s = "This is a nice string";
+    //But declaring it as an array is different. The compiler doesn’t stow those bytes in another part of town, they’re right down the street. This one is a mutable copy of the string – one we can change at will:
+    char se[] = "This is a nice string as well";
+    printf("%s\n", s);
+    printf("%c\n", s[0]);
+    printf("%c\n", se[0]);
+    print_continues();
+    //undefined behavior will produce a crash
+    //s[0] = 'E';
+
+    //this actually works
+    se[0] = 'E';
+    printf("%c\n", s[0]);
+    printf("%c\n", se[0]);
+    print_continues();
+    printf("Length of se is: %zu\n", strlen(se));
+    print_continues();
+    printf("Length of se is: %d\n", my_strlen(se));
+    print_continues();
+    char sw[] = "Hello, world!";
+    printf("%zu\n", sizeof(sw));
+    char t[sizeof(sw)];
+    strcpy(t, sw);
+    t[0] = 'z';
+    // s remains unaffected
+    printf("%s\n", sw);
+    // But t has been changed
+    printf("%s\n", t);
+}
+
+int my_strlen(char *s) {
+    int count = 0;
+    while (s[count] != '\0') { count++;}
+    return count;
+}
+
